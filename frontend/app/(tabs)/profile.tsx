269d8978decc9947b204086da6c2538691e54ec8
Dylan Chaperon <dylan.chaperon@epita.fr>
2025-12-16 21:51:26 +0100

    [FIX clc

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -6,7 +6,7 @@ import { ThemedView } from "@components/ThemedView";
 import { Image } from "expo-image";
 
 import { ProfileHeader } from "@components/profile/ProfileHeader";
-import { Card } from "@components/profile/Card";
+import { Card } from "@components/profile/Cards";
 import { RowItem } from "@components/ui/RowItem";
 import { SectionHeader } from "@components/ui/SectionHeader";
 
