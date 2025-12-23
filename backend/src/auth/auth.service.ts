ca59fc51e438a1c9758050ac64a4a06a8edb9d6c
maxence.beunel <maxence.beunel@epita.fr>
2025-12-23 13:19:51 +0100

    remove useless select

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -35,9 +35,8 @@ export class AuthService {
         password: hashed,
         birthDate: iso8601ToDateTime(dto.birthDate),
       },
-      omit: {
-        password: true,
-        refreshToken: true,
+      select: {
+        id: true,
       },
     });
 
