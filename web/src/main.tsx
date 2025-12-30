e6524b46ee7c05f09b4a7293eb0b8a9a9bd96a5f
Dylan Chaperon <dylan.chaperon@epita.fr>
2025-12-30 18:22:23 +0100

    fix: apply prettier

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,12 +1,12 @@
+import App from "@/app/App";
+import { AuthProvider } from "@/contexts/AuthProvider";
+import { ThemeProvider } from "@/contexts/ThemeProvider";
+import "@/index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "@/app/App";
 import { Toaster } from "sonner";
-import "@/index.css";
-import { AuthProvider } from "@/contexts/AuthProvider";
-import { ThemeProvider } from "@/contexts/ThemeProvider";
 
 const queryClient = new QueryClient();
 
