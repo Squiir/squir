4f487e0e9ff5161163d31f33144d3bfc14beae51
dylanchpr <71972190+dylanchpr@users.noreply.github.com>
2026-02-04 11:11:16 +0100

    Update web/src/pages/bars/[id].tsx

--- web/src/pages/bars/[id].tsx
+++ web/src/pages/bars/[id].tsx
@@ -22,7 +22,7 @@ export default function BarDetailPage() {
   const handleBack = () => {
     navigate(-1);
   };
-
+// TODO: ajouter le tag & les horaires aux établissements et remplacer les mock data
   const isBarFavorite = id ? isVenueFavorite(id) : false;
 
   const handleFavoriteBar = () => {
